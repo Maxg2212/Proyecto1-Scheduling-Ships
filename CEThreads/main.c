7cd32abb8583bd02c9a17c065aab4cc1c9adc794
Michael V <maicolvn1399@gmail.com>
2024-10-07 23:21:40 -0600

    [Update] test of threads and mutex

--- CEThreads/main.c
+++ CEThreads/main.c
@@ -1,13 +1,40 @@
+
+
 #include "CEthread.h"
 
+
+#include <pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+
 
 // Variable global del contador
 int counter = 0;
-pthread_mutex_t lock; 
 
+CEthread_t tid[2];
+int counter;
+CEthread_mutex_t *lock;
+
+
+
+void* trythis(void* arg)
+{
+    CEmutex_trylock(lock);
+
+    unsigned long i = 0;
+    counter += 1;
+    printf("\n Job %d has started\n", counter);
+
+    for (i = 0; i < (0xFFFFFFFF); i++);
+
+    printf("\n Job %d has finished\n", counter);
+
+    CEmutex_unlock(lock);
+
+    return NULL;
+}
 
 // Función que incrementa el contador
 int increment_counter(void* args) {
@@ -29,24 +56,6 @@ int hey(void* args){
     return 0;
 }
 
-void* trythis(void* arg)
-{
-    pthread_mutex_lock(&lock);
-
-    unsigned long i = 0;
-    counter += 1;
-    printf("\n Job %d has started\n", counter);
-
-    for (i = 0; i < (0xFFFFFFFF); i++)
-        ;
-
-    printf("\n Job %d has finished\n", counter);
-
-    pthread_mutex_unlock(&lock);
-
-    return NULL;
-}
-
 void creation_test(){
 
     printf("\n\n ------- Now testing creation and killing ------ \n\n");
@@ -61,8 +70,78 @@ void creation_test(){
 
 }
 
+int no_mutex_count(void* args) {
+
+    sleep(1);
+
+    int pid = getpid();
+
+    printf("Hey its me child pid %i, im gonna print 5 times my pid\n", pid);
+
+    // Lock the mutex
+    for (size_t i = 0; i < 5; ++i)
+    {
+        printf("%i\n", pid);
+        sleep(1);
+    }
+    printf("Done \n\n");
+
+    return 0;
+}
+
+
+int mutex_count(void* args) {
+    CEthread_mutex_t* mutex = (CEthread_mutex_t*) args;
+
+    sleep(1);
+
+    int pid = getpid();
+
+    // Lock the mutex
+    CEmutex_trylock(mutex);
+
+    printf("Hey its me child pid %i, im gonna print 5 times my pid\n", pid);
+
+    for (size_t i = 0; i < 5; ++i)
+    {
+        printf("%i\n", pid);
+        sleep(1);
+    }
+    printf("Done \n\n");
+    CEmutex_unlock(mutex);
+
+    return 0;
+}
+
+
+void mutex_test(){
+    printf("\n\n ------- Now testing non mutex vs mutex printing ------ \n\n");
+    CEthread_t* thread1;
+    CEthread_t* thread2;
+    CEthread_mutex_t* mutex;
+
+    // Create mutex
+    CEmutex_init(&mutex, NULL);
+
+    // Launch non mutex threads
+    printf("Thread 1 created with pid %i\n", CEthread_create(&thread1, NULL, &no_mutex_count, NULL));
+    printf("Thread 2 created with pid %i\n", CEthread_create(&thread2, NULL, &no_mutex_count, NULL));
+
+    sleep(6);
+    // Launch mutex threads
+    printf("Thread 1 created with pid %i\n", CEthread_create(&thread1, NULL, &mutex_count, mutex));
+    printf("Thread 2 created with pid %i\n", CEthread_create(&thread2, NULL, mutex_count, mutex));
+
+    sleep(12);
+}
+
 int main() {
 
+
+    //creation_test();
+    mutex_test();
+
+    /**
     CEthread_t* thread;
     CEthread_t* thread2;
 
@@ -71,6 +150,7 @@ int main() {
 
     CEthread_join(thread);
     CEthread_join(thread2);
+    */
 
 
     return 0;
